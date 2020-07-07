a8bf9d139e9949dfb7cf689522da9d5d07f9ae25
徐新华 <polaris@studygolang.com>
2020-07-07 18:48:36 +0800

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+git clone https://github.com/chai2010/advanced-go-programming-book
+
+git clone https://github.com/yeasy/docker_practice
+git clone https://github.com/ffhelicopter/Go42
+git clone https://github.com/achun/Go-Blog-In-Action
+git clone https://github.com/gobyexample-cn/gobyexample
+git clone https://github.com/RainbowMango/GoExpertProgramming
+git clone https://github.com/tmrts/go-patterns
+git clone https://github.com/gopl-zh/gopl-zh.github.com
+git clone https://github.com/jasperxu/gorm-zh
+git clone https://github.com/smallnest/go-rpc-programming-guide
+git clone https://github.com/changkun/go-under-the-hood
+git clone https://github.com/go-chinese-site/gowebexamples
+git clone https://github.com/studygolang/learn-go-with-tests
+git clone https://github.com/hantmac/Mastering_Go_ZH_CN
+git clone https://github.com/astaxie/NPWG_zh
+git clone https://github.com/polaris1119/The-Golang-Standard-Library-by-Example
+git clone https://github.com/songleo/the-little-go-book_ZH_CN
+git clone https://github.com/Unknwon/the-way-to-go_ZH_CN
+git clone https://gitea.com/xorm/manual-zh-CN xorm
